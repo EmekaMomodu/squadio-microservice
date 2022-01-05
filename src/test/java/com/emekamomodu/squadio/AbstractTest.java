1e7341ef9cf879b6528103fd6d0bbc36e1c86525
Chukwuemeka Momodu <momodu.ceo@gmail.com>
2022-01-05 21:36:58 +0100

    unit test for login success with admin user

---
+++ src/test/java/com/emekamomodu/squadio/AbstractTest.java
@@ -0,0 +1,47 @@
+package com.emekamomodu.squadio;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+
+/**
+ * @author CMOMODU
+ * @version 1.0
+ * @date 1/5/22 6:28 PM
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = SquadioApplication.class)
+@WebAppConfiguration
+public class AbstractTest {
+
+    protected MockMvc mockMvc;
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    protected void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    protected String mapToJson(Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(obj);
+    }
+
+    protected <T> T mapFromJson(String json, Class<T> clazz)
+            throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, clazz);
+    }
+
+}
